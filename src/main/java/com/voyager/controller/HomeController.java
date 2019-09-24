d704b5b014ee63b37b98d83eb03ba0e0429c20af
Pasindu Piumal <pasindu0996@gmail.com>
2019-09-25 05:18:58 +0530

    Project restructuring

---
+++ src/main/java/com/voyager/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.voyager.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.istack.internal.logging.Logger;
+
+public class HomeController extends HttpServlet {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = Logger.getLogger(HomeController.class);
+	
+	public HomeController() {
+		super();
+	}
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		LOGGER.log(Level.INFO, "Control aquired by Home Controller. Re-directing control to /Routes");
+
+		//Re-direct to the RouteController servlet
+		response.sendRedirect(request.getContextPath() + "/Routes");
+	}
+
+}
---
+++ src/main/java/com/voyager/controller/RouteController.java
@@ -0,0 +1,35 @@
+package com.voyager.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.istack.internal.logging.Logger;
+import com.voyager.database.DBConnection;
+
+
+public class RouteController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = Logger.getLogger(RouteController.class);
+
+    public RouteController() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		LOGGER.log(Level.INFO, "Control aquired by Route Controller. Dispatching RouteManagement JSP");
+		
+		final RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/RouteManagement.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
